86d4e7d538389b5c994c2c5fdc8f5457d4193bba
Bill <136879108+D0wnfall@users.noreply.github.com>
2024-06-30 22:25:53 +0700

    asset update, food random
    update beberapa asset sama randomizer food

--- snake-linkedlist.py
+++ snake-linkedlist.py
@@ -2,13 +2,13 @@ import pygame
 import sys
 import random
 
-# Inisialisasi kelas Node
+# Inisialiasi kelas Node
 class Node:
     def __init__(self, position):
         self.position = position
         self.next = None
 
-# Inisialisasi kelas snake berisi method-method untuk manipulasi snake
+# inisialisasi kelas snake berisi method-method untuk manipulasi snake
 class Snake:
     def __init__(self, position):
         self.head = Node(position)
@@ -41,20 +41,22 @@ class Snake:
             self.tail = current
         self.length -= 1
 
-# Inisialisasi pygame
+# inisialiasi pygame
 pygame.init()
 
-# Atur ukuran window
+# ngatur ukuran window
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 500
 GRID_SIZE = 30
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Masukkan aset gambar
+#masukin aset-aset gambar
 judul = pygame.image.load('Assets/judul.png')
 apple_image = pygame.image.load('Assets/buah/apple.png')
 apple_image = pygame.transform.scale(apple_image, (GRID_SIZE, GRID_SIZE))
+banana_image = pygame.image.load('Assets/buah/banana.png')
+banana_image = pygame.transform.scale(banana_image, (GRID_SIZE, GRID_SIZE))
 kulit_pisang = pygame.image.load('Assets/sampah/kulitPisang.png')
 kulit_pisang = pygame.transform.scale(kulit_pisang, (GRID_SIZE, GRID_SIZE))
 dirty = pygame.image.load('Assets/sampah/dirty.png')
@@ -66,11 +68,11 @@ applesisa = pygame.transform.scale(applesisa, (GRID_SIZE, GRID_SIZE))
 batu = pygame.image.load('Assets/obstacle/batu.png')
 batu = pygame.transform.scale(batu, (GRID_SIZE, GRID_SIZE))
 
-# Masukkan suara
+#masukin suara
 crunch_sound = pygame.mixer.Sound('Sound/crunch.wav')
 die_sound = pygame.mixer.Sound('Sound/mixkit-little-piano-game-over-1944.wav')
 
-# Kelas utama untuk game
+#Kelas utama untuk game
 class SnakeGame:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -79,27 +81,28 @@ class SnakeGame:
         self.trash_positions = []
         self.trash_timer = 0
         self.food_timer = 0
+        self.buah_images = [apple_image, banana_image]
+        self.buah_items = []
         self.trash_images = [kulit_pisang, dirty, sampah, applesisa]
         self.trash_items = []
-        self.obstacle_positions = []
         self.reset()
         
     def reset(self):
         self.snake = Snake((GRID_WIDTH // 2, GRID_HEIGHT // 2))
         self.trash_positions = []
         self.trash_items = []
-        self.obstacle_positions = [self.getRandomObstaclePos() for _ in range(3)]
         self.trash_timer = 0
         self.food_timer = pygame.time.get_ticks()
         self.snake_direction = (1, 0)
-        self.food_position = self.getRandomFoodPos()
+        self.food_position, self.current_food_image = self.getRandomFood()
         self.score = 0
     
-    def getRandomFoodPos(self):
+    def getRandomFood(self):
         while True:
             position = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-            if not self.isEatenBySnake(position) and position not in self.obstacle_positions:
-                return position
+            if not self.isEatenBySnake(position):
+                image = random.choice(self.buah_images)
+                return position, image
 
     def isEatenBySnake(self, position):
         current = self.snake.head
@@ -115,13 +118,6 @@ class SnakeGame:
     def getRandomTrashPos(self):
         while True:
             position = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-            if (not self.isEatenBySnake(position) and position != self.food_position 
-                    and position not in self.obstacle_positions):
-                return position
-
-    def getRandomObstaclePos(self):
-        while True:
-            position = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
             if not self.isEatenBySnake(position) and position != self.food_position:
                 return position
 
@@ -131,14 +127,14 @@ class SnakeGame:
             (self.snake.head.position[1] + self.snake_direction[1]) % GRID_HEIGHT
         )
         
-        if self.isEatenBySnake(new_head_position) or new_head_position in self.obstacle_positions:
+        if self.isEatenBySnake(new_head_position):
             self.play_die_sound()
             self.reset()
         else:
             self.snake.addHead(new_head_position)
             
             if new_head_position == self.food_position:
-                self.food_position = self.getRandomFoodPos()
+                self.food_position, self.current_food_image = self.getRandomFood()
                 self.play_crunch_sound()
                 self.score += 1
                 self.food_timer = pygame.time.get_ticks()
@@ -159,7 +155,7 @@ class SnakeGame:
                 self.snake.removeTail()
 
         if pygame.time.get_ticks() - self.food_timer > 5000:
-            self.food_position = self.getRandomFoodPos()
+            self.food_position, self.current_food_image = self.getRandomFood()
             self.food_timer = pygame.time.get_ticks()
 
     def draw(self):
@@ -174,13 +170,10 @@ class SnakeGame:
             )
             current = current.next
         
-        self.screen.blit(apple_image, (self.food_position[0] * GRID_SIZE, self.food_position[1] * GRID_SIZE))
+        self.screen.blit(self.current_food_image, (self.food_position[0] * GRID_SIZE, self.food_position[1] * GRID_SIZE))
         
         for index, position in enumerate(self.trash_positions):
             self.screen.blit(self.trash_items[index], (position[0] * GRID_SIZE, position[1] * GRID_SIZE))
-        
-        for position in self.obstacle_positions:
-            self.screen.blit(batu, (position[0] * GRID_SIZE, position[1] * GRID_SIZE))
         
         self.draw_score()
         pygame.display.flip()
@@ -206,7 +199,7 @@ class SnakeGame:
             font_color = (0, 0, 0)
             font = pygame.font.Font(None, 36)
             scaled_judul = pygame.transform.scale(judul, (290, 200))
-            self.screen.blit(scaled_judul, (SCREEN_WIDTH//2-150, 20))
+            self.screen.blit(scaled_judul, (SCREEN_WIDTH // 2 - 150, 20))
             score_text = font.render("Press any key to start", True, font_color)
             name1 = font.render("Crisnanda", True, font_color)
             name2 = font.render("Victor", True, font_color)
@@ -216,10 +209,10 @@ class SnakeGame:
             name1_rect = name1.get_rect()
             name2_rect = name2.get_rect()
             name3_rect = name3.get_rect()
-            name1_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT//2-40)
-            name2_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT//2-15)
-            name3_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT//2+10)
-            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT//2+100)
+            name1_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 40)
+            name2_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 15)
+            name3_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 10)
+            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
           
             self.screen.blit(score_text, text_rect)
             self.screen.blit(name1, name1_rect)
